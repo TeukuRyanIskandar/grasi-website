256a960fd5ae31f95fca4f14c034cc4f5dea3e29
Ryan Iskandar <teukurian090598@gmail.com>
2024-05-10 16:18:37 +0800

    Outline of a footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="footer-wrapper">
+            <div className="footer-content-wrapper">
+
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
